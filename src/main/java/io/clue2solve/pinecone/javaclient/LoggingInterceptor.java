6ac90a29e2ce0ae89e55a252fcc4ffd33d74a820
honnuanand <anand@honnu.com>
2023-11-16 14:35:31 -0800

    Pretty Pring JSON in the Logs

--- src/main/java/io/clue2solve/pinecone/javaclient/LoggingInterceptor.java
+++ src/main/java/io/clue2solve/pinecone/javaclient/LoggingInterceptor.java
@@ -1,12 +1,11 @@
 package io.clue2solve.pinecone.javaclient;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import okhttp3.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import okhttp3.Interceptor;
-import okhttp3.MediaType;
-import okhttp3.Request;
-import okhttp3.Response;
-import okhttp3.ResponseBody;
 import okio.Buffer;
 
 import java.io.IOException;
@@ -22,7 +21,10 @@ public class LoggingInterceptor implements Interceptor {
         Request request = chain.request();
 
         long t1 = System.nanoTime();
-        logger.info("Sending request {} on {}\n{}", request.url(), chain.connection(), request.headers());
+        Headers headers = request.headers().newBuilder()
+            .removeAll("Api-Key") // remove the Api-Key header
+            .build();
+        logger.info("Sending request {} on {}\n{}", request.url(), chain.connection(), headers);
 
         String requestBody = null;
         if (request.body() != null) {
@@ -35,13 +37,19 @@ public class LoggingInterceptor implements Interceptor {
         Response response = chain.proceed(request);
 
         long t2 = System.nanoTime();
-        logger.info("Received response for {} in {:.1f}ms\n{}", response.request().url(), (t2 - t1) / 1e6, response.headers());
+        Headers responseHeaders = response.headers().newBuilder()
+            .removeAll("Api-Key") // remove the Api-Key header
+            .build();
+        logger.info("Received response for {} in {:.1f}ms\n{}", response.request().url(), (t2 - t1) / 1e6, responseHeaders);
 
         MediaType contentType = response.body().contentType();
         String content = response.body().string(); // Only read once
-        logger.info("Response body: {}", content);
+        ObjectMapper mapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
+        String prettyContent = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(mapper.readValue(content, Object.class));
+        logger.info("Response body: {}", prettyContent);
 
         ResponseBody wrappedBody = ResponseBody.create(contentType, content);
+        //noinspection KotlinInternalInJava
         return response.newBuilder().body(wrappedBody).build();
     }
 }
